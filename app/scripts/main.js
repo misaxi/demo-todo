2d19295fe18d7a6babe422b23e205036c02a754b
mike he <misaxi@gmail.com>
2015-10-25 10:20:19 +1100

    bean.color === 'brown' // haha, let's do iit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,9 @@
 require('angular')
   .module('todo', [require('angular-material')])
+  .config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('brown')
+      .accentPalette('brown')
+  })
 
 require('./controllers')
